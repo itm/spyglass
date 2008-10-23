dbb006fd1925821e6b9d4abe0aa3fab3bd4892c1
Daniel Bimschas <bimschas@itm.uni-luebeck.de>
2008-10-23 13:04:05 +0000

    Removed testcases for Dariush

--- test/de/uniluebeck/itm/spyglass/xmlconfig/PluginXMLConfigTests.java
+++ test/de/uniluebeck/itm/spyglass/xmlconfig/PluginXMLConfigTests.java
@@ -4,7 +4,6 @@
  */
 package de.uniluebeck.itm.spyglass.xmlconfig;
 
-import org.eclipse.core.runtime.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,24 +20,6 @@ public class PluginXMLConfigTests {
 	 */
 	@Before
 	public void setUp() throws Exception {
-	}
-	
-	/**
-	 * Test method for
-	 * {@link de.uniluebeck.itm.spyglass.xmlconfig.PluginXMLConfig#getAllSemanticTypes()}.
-	 */
-	@Test
-	public void testIsAllSemanticTypes() {
-		final PluginXMLConfig config = new PluginXMLConfigStub();
-		config.setSemanticTypes(PluginXMLConfig.ALL_SEMANTIC_TYPES);
-		Assert.isTrue(config.getAllSemanticTypes());
-	}
-	
-	@Test
-	public void testIsAllSemanticTypes2() {
-		final PluginXMLConfig config = new PluginXMLConfigStub();
-		config.setSemanticTypes(new int[] { 1, 2, 3, 4, 5 });
-		Assert.isTrue(!config.getAllSemanticTypes());
 	}
 	
 	@Test
