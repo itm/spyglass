9ac88b11d965e94cc5166da76ebb73e5f9cf14fc
Daniel Bimschas <bimschas@itm.uni-luebeck.de>
2008-12-18 14:34:21 +0000

    Lock im GridPainterOptionsComposite

--- src/de/uniluebeck/itm/spyglass/plugin/gridpainter/GridPainterOptionsComposite.java
+++ src/de/uniluebeck/itm/spyglass/plugin/gridpainter/GridPainterOptionsComposite.java
@@ -1,5 +1,6 @@
 package de.uniluebeck.itm.spyglass.plugin.gridpainter;
 
+import org.eclipse.core.databinding.Binding;
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.databinding.UpdateValueStrategy;
 import org.eclipse.core.databinding.beans.BeansObservables;
@@ -70,6 +71,18 @@ public class GridPainterOptionsComposite extends Composite {
 
 	private Label gridElementHeightUnitLabel;
 
+	private Button buttonLockGridElementsSquare;
+
+	private GridPainterXMLConfig config;
+
+	private Binding lockBindingElementsSquare;
+
+	private DataBindingContext dbc;
+
+	private Binding lockBindingNumberOfRowsNCols;
+
+	private Button buttonLockNumberOfRowsNCols;
+
 	public GridPainterOptionsComposite(final Composite parent) {
 		super(parent, SWT.NONE);
 		initGUI();
@@ -99,142 +112,326 @@ public class GridPainterOptionsComposite extends Composite {
 			group.setText("Grid");
 
 			{
-				// lower left point
-				data = new GridData();
-				data.widthHint = 100;
+				{
+					// 1st row
+					{
+						// 1st column
+						// lower left point
+						data = new GridData();
+						data.widthHint = 120;
 
-				label = new Label(group, SWT.NONE);
-				label.setText("Lower Left Point X:");
-				label.setLayoutData(data);
-
-				data = new GridData();
-				data.widthHint = 40;
-
-				lowerLeftPointXText = new Text(group, SWT.BORDER);
-				lowerLeftPointXText.setLayoutData(data);
-
-				lowerLeftPointXUnitLabel = new Label(group, SWT.NONE);
-
-				data = new GridData();
-				data.verticalSpan = 4;
-				data.widthHint = 30;
-
-				label = new Label(group, SWT.NONE);
-				label.setText("");
-				label.setLayoutData(data);
-
-				data = new GridData();
-				data.widthHint = 100;
-
-				label = new Label(group, SWT.NONE);
-				label.setText("Lower Left Point Y:");
-				label.setLayoutData(data);
-
-				data = new GridData();
-				data.widthHint = 40;
-
-				lowerLeftPointYText = new Text(group, SWT.BORDER);
-				lowerLeftPointYText.setLayoutData(data);
-
-				lowerLeftPointYUnitLabel = new Label(group, SWT.NONE);
-
-				label = new Label(group, SWT.NONE);
-				label.setText("");
-
-				// rows and columns
-				label = new Label(group, SWT.NONE);
-				label.setText("# Rows");
-
-				data = new GridData();
-				data.widthHint = 40;
-
-				numRowsText = new Text(group, SWT.BORDER);
-				numRowsText.setLayoutData(data);
-
-				label = new Label(group, SWT.NONE);
-				label.setText("");
-
-				label = new Label(group, SWT.NONE);
-				label.setText("# Columns");
-
-				data = new GridData();
-				data.widthHint = 40;
-
-				numColsText = new Text(group, SWT.BORDER);
-				numColsText.setLayoutData(data);
-
-				label = new Label(group, SWT.NONE);
-				label.setText("");
-
-				lockNumberOfRowsNColsCheckbox = new Button(group, SWT.CHECK);
-				lockNumberOfRowsNColsCheckbox.setText("lock");
-
-				// row width and column height
-				label = new Label(group, SWT.NONE);
-				label.setText("Row Width");
-
-				data = new GridData();
-				data.widthHint = 40;
-
-				gridElementWidthText = new Text(group, SWT.BORDER);
-				gridElementWidthText.setLayoutData(data);
-
-				gridElementWidthUnitLabel = new Label(group, SWT.NONE);
-
-				label = new Label(group, SWT.NONE);
-				label.setText("Column Height");
-
-				data = new GridData();
-				data.widthHint = 40;
-
-				gridElementHeightText = new Text(group, SWT.BORDER);
-				gridElementHeightText.setLayoutData(data);
-
-				gridElementHeightUnitLabel = new Label(group, SWT.NONE);
-
-				lockGridElementsSquareCheckbox = new Button(group, SWT.CHECK);
-				lockGridElementsSquareCheckbox.setText("lock");
-
-				label = new Label(group, SWT.NONE);
-				label.setText("Line Width");
-
-				data = new GridData();
-				data.widthHint = 40;
-				data.horizontalSpan = 2;
-
-				lineWidth = new Text(group, SWT.BORDER);
-				lineWidth.setLayoutData(data);
-
-				label = new Label(group, SWT.NONE);
-				label.setText("Line Color");
-
-				data = new GridData();
-				data.widthHint = 50;
-				data.heightHint = 19;
-
-				colorExample = new CLabel(group, SWT.BORDER);
-				colorExample.setLayoutData(data);
-
-				data = new GridData();
-				data.horizontalSpan = 2;
-
-				lineColor = new Button(group, SWT.PUSH | SWT.CENTER);
-				lineColor.setText("Change color");
-				lineColor.setLayoutData(data);
-				lineColor.addSelectionListener(new SelectionAdapter() {
-					@Override
-					public void widgetSelected(final SelectionEvent evt) {
-						final ColorDialog dlg = new ColorDialog(getShell());
-						dlg.setRGB(colorExample.getBackground().getRGB());
-						final RGB color = dlg.open();
-						if (color != null) {
-							colorExample.setBackground(new Color(getDisplay(), color));
-							page.markFormDirty();
-						}
+						label = new Label(group, SWT.NONE);
+						label.setText("Lower Left Point:");
+						label.setLayoutData(data);
 					}
-				});
+					{
+						// 2nd column
+						data = new GridData();
+						data.widthHint = 20;
+
+						label = new Label(group, SWT.NONE);
+						label.setText("x:");
+						label.setLayoutData(data);
+					}
+					{
+						// 3rd column
+						data = new GridData();
+						data.widthHint = 40;
+
+						lowerLeftPointXText = new Text(group, SWT.BORDER);
+						lowerLeftPointXText.setLayoutData(data);
+					}
+					{
+						// 4th column
+						lowerLeftPointXUnitLabel = new Label(group, SWT.NONE);
+						lowerLeftPointXUnitLabel.setText("");
+					}
+					{
+						// 5th column
+						data = new GridData();
+						data.widthHint = 30;
+
+						label = new Label(group, SWT.NONE);
+						label.setText("");
+						label.setLayoutData(data);
+					}
+					{
+						// 6th column
+						data = new GridData();
+						data.widthHint = 20;
+
+						label = new Label(group, SWT.NONE);
+						label.setText("y:");
+						label.setLayoutData(data);
+					}
+					{
+						// 7th column
+						data = new GridData();
+						data.widthHint = 40;
+
+						lowerLeftPointYText = new Text(group, SWT.BORDER);
+						lowerLeftPointYText.setLayoutData(data);
+					}
+					{
+						// 8th column
+						lowerLeftPointYUnitLabel = new Label(group, SWT.NONE);
+						lowerLeftPointYUnitLabel.setText("");
+						lowerLeftPointYUnitLabel.setLayoutData(new GridData());
+					}
+				}
+				{
+					// 2nd row
+					{
+						// 1st column
+						// rows and columns
+						data = new GridData();
+						data.horizontalSpan = 2;
+
+						label = new Label(group, SWT.NONE);
+						label.setText("# Rows / Columns:");
+						label.setLayoutData(data);
+					}
+					{
+						// 3rd column
+						data = new GridData();
+						data.widthHint = 40;
+						data.horizontalSpan = 2;
+
+						numRowsText = new Text(group, SWT.BORDER);
+						numRowsText.setLayoutData(data);
+					}
+					{
+						// 4th to 6th column
+						data = new GridData();
+						data.widthHint = 50;
+						data.heightHint = 20;
+						data.horizontalSpan = 2;
+
+						buttonLockNumberOfRowsNCols = new Button(group, SWT.PUSH | SWT.CENTER);
+						buttonLockNumberOfRowsNCols.setLayoutData(data);
+						buttonLockNumberOfRowsNCols.setImage(SWTResourceManager
+								.getImage("de/uniluebeck/itm/spyglass/gui/configuration/chain_small_closed_hor.png"));
+
+						buttonLockNumberOfRowsNCols.addSelectionListener(new SelectionAdapter() {
+							@Override
+							public void widgetSelected(final SelectionEvent evt) {
+
+								/*
+								 * if pressed, change image/text and change binding between both
+								 * text fields (add binding / remove binding
+								 */
+								final boolean currentlyLocked = config.getLockNumberOfRowsNCols();
+								config.setLockNumberOfRowsNCols(!currentlyLocked);
+								updateLockNumberOfRowsNCols();
+
+							}
+
+						});
+					}
+					{
+						// 7th to 8th column
+						data = new GridData();
+						data.widthHint = 40;
+						data.horizontalSpan = 2;
+
+						numColsText = new Text(group, SWT.BORDER);
+						numColsText.setLayoutData(data);
+					}
+				}
+				{
+					// 3rd row
+					{
+						// 1st column
+						// row width and column height
+						data = new GridData();
+						data.horizontalSpan = 2;
+
+						label = new Label(group, SWT.NONE);
+						label.setText("Row Width / Column Height:");
+						label.setLayoutData(data);
+					}
+					{
+						// 2nd column
+						data = new GridData();
+						data.widthHint = 40;
+
+						gridElementWidthText = new Text(group, SWT.BORDER);
+						gridElementWidthText.setLayoutData(data);
+					}
+					{
+						// 3rd column
+						gridElementWidthUnitLabel = new Label(group, SWT.NONE);
+						gridElementWidthUnitLabel.setLayoutData(new GridData());
+					}
+					{
+						// 4th to 6th column
+						data = new GridData();
+						data.widthHint = 50;
+						data.heightHint = 20;
+						data.horizontalSpan = 2;
+
+						buttonLockGridElementsSquare = new Button(group, SWT.PUSH | SWT.CENTER);
+						buttonLockGridElementsSquare.setLayoutData(data);
+						buttonLockGridElementsSquare.setImage(SWTResourceManager
+								.getImage("de/uniluebeck/itm/spyglass/gui/configuration/chain_small_closed_hor.png"));
+
+						buttonLockGridElementsSquare.addSelectionListener(new SelectionAdapter() {
+							@Override
+							public void widgetSelected(final SelectionEvent evt) {
+
+								/*
+								 * if pressed, change image/text and change binding between both
+								 * text fields (add binding / remove binding
+								 */
+								final boolean currentlyLocked = config.getLockGridElementsSquare();
+								config.setLockGridElementsSquare(!currentlyLocked);
+								updateGridElementsSquare();
+
+							}
+
+						});
+					}
+					{
+						// 7th column
+						data = new GridData();
+						data.widthHint = 40;
+
+						gridElementHeightText = new Text(group, SWT.BORDER);
+						gridElementHeightText.setLayoutData(data);
+					}
+					{
+						// 8th column
+						gridElementHeightUnitLabel = new Label(group, SWT.NONE);
+						gridElementHeightUnitLabel.setLayoutData(new GridData());
+					}
+				}
+				{
+					// 4th row
+					{
+						// 1st column
+						data = new GridData();
+						data.horizontalSpan = 2;
+
+						label = new Label(group, SWT.NONE);
+						label.setText("Line Width");
+						label.setLayoutData(data);
+					}
+					{
+						// 2nd to 8th column
+						data = new GridData();
+						data.widthHint = 40;
+						data.horizontalSpan = 6;
+
+						lineWidth = new Text(group, SWT.BORDER);
+						lineWidth.setLayoutData(data);
+					}
+				}
+				{
+					// 5th row
+					{
+						// 1st column
+						data = new GridData();
+						data.horizontalSpan = 2;
+
+						label = new Label(group, SWT.NONE);
+						label.setText("Line Color");
+						label.setLayoutData(data);
+					}
+					{
+						// 2nd column
+						data = new GridData();
+						data.widthHint = 50;
+						data.heightHint = 19;
+
+						colorExample = new CLabel(group, SWT.BORDER);
+						colorExample.setLayoutData(data);
+					}
+					{
+						// 3rd to 8th column
+						data = new GridData();
+						data.horizontalSpan = 5;
+
+						lineColor = new Button(group, SWT.PUSH | SWT.CENTER);
+						lineColor.setText("Change color");
+						lineColor.setLayoutData(data);
+						lineColor.addSelectionListener(new SelectionAdapter() {
+							@Override
+							public void widgetSelected(final SelectionEvent evt) {
+								final ColorDialog dlg = new ColorDialog(getShell());
+								dlg.setRGB(colorExample.getBackground().getRGB());
+								final RGB color = dlg.open();
+								if (color != null) {
+									colorExample.setBackground(new Color(getDisplay(), color));
+									page.markFormDirty();
+								}
+							}
+						});
+					}
+
+				}
 
 			}
+
+		}
+	}
+
+	private void updateLockNumberOfRowsNCols() {
+
+		final boolean locked = config.getLockNumberOfRowsNCols();
+
+		if (locked) {
+
+			numColsText.setText(numRowsText.getText());
+
+			if (lockBindingNumberOfRowsNCols == null) {
+
+				// bind the two fields together
+				lockBindingNumberOfRowsNCols = dbc.bindValue(SWTObservables.observeText(numRowsText, SWT.Modify), SWTObservables.observeText(
+						numColsText, SWT.Modify), null, null);
+			}
+
+			buttonLockNumberOfRowsNCols.setImage(SWTResourceManager
+					.getImage("de/uniluebeck/itm/spyglass/gui/configuration/chain_small_closed_hor.png"));
+
+		} else {
+			if (lockBindingNumberOfRowsNCols != null) {
+				// Kill the binding (it will be automatically removed from the dbc)
+				lockBindingNumberOfRowsNCols.dispose();
+				lockBindingNumberOfRowsNCols = null;
+			}
+
+			buttonLockNumberOfRowsNCols
+					.setImage(SWTResourceManager.getImage("de/uniluebeck/itm/spyglass/gui/configuration/chain_small_open_hor.png"));
+
+		}
+	}
+
+	private void updateGridElementsSquare() {
+
+		final boolean locked = config.getLockGridElementsSquare();
+
+		if (locked) {
+			gridElementHeightText.setText(gridElementWidthText.getText());
+
+			if (lockBindingElementsSquare == null) {
+
+				// bind the two fields together
+				lockBindingElementsSquare = dbc.bindValue(SWTObservables.observeText(gridElementWidthText, SWT.Modify), SWTObservables.observeText(
+						gridElementHeightText, SWT.Modify), null, null);
+			}
+
+			buttonLockGridElementsSquare.setImage(SWTResourceManager
+					.getImage("de/uniluebeck/itm/spyglass/gui/configuration/chain_small_closed_hor.png"));
+
+		} else {
+			if (lockBindingElementsSquare != null) {
+				// Kill the binding (it will be automatically removed from the dbc)
+				lockBindingElementsSquare.dispose();
+				lockBindingElementsSquare = null;
+			}
+
+			buttonLockGridElementsSquare.setImage(SWTResourceManager
+					.getImage("de/uniluebeck/itm/spyglass/gui/configuration/chain_small_open_hor.png"));
 
 		}
 	}
@@ -243,6 +440,8 @@ public class GridPainterOptionsComposite extends Composite {
 			final Spyglass spyglass) {
 
 		this.page = page;
+		this.config = config;
+		this.dbc = dbc;
 
 		IObservableValue obsModel;
 		ISWTObservableValue obsWidget;
@@ -289,18 +488,6 @@ public class GridPainterOptionsComposite extends Composite {
 			dbc.bindValue(obsWidget, obsModel, usTargetToModel, usModelToTarget);
 		}
 		{
-			obsWidget = SWTObservables.observeSelection(lockGridElementsSquareCheckbox);
-			obsModel = BeansObservables.observeValue(dbc.getValidationRealm(), config, GridPainterXMLConfig.PROPERTYNAME_LOCK_GRID_ELEMENTS_SQUARE);
-			usTargetToModel = new UpdateValueStrategy(UpdateValueStrategy.POLICY_CONVERT);
-			dbc.bindValue(obsWidget, obsModel, usTargetToModel, null);
-		}
-		{
-			obsWidget = SWTObservables.observeSelection(lockNumberOfRowsNColsCheckbox);
-			obsModel = BeansObservables.observeValue(dbc.getValidationRealm(), config, GridPainterXMLConfig.PROPERTYNAME_LOCK_NUMBER_OF_ROWS_N_COLS);
-			usTargetToModel = new UpdateValueStrategy(UpdateValueStrategy.POLICY_CONVERT);
-			dbc.bindValue(obsWidget, obsModel, usTargetToModel, null);
-		}
-		{
 			obsWidget = SWTObservables.observeText(numColsText, SWT.Modify);
 			obsModel = BeansObservables.observeValue(dbc.getValidationRealm(), config, GridPainterXMLConfig.PROPERTYNAME_NUM_COLS);
 			usTargetToModel = new UpdateValueStrategy(UpdateValueStrategy.POLICY_CONVERT);
@@ -321,6 +508,9 @@ public class GridPainterOptionsComposite extends Composite {
 			dbc.bindValue(SWTObservables.observeText(gridElementWidthUnitLabel), obsModel, null, null);
 			dbc.bindValue(SWTObservables.observeText(gridElementHeightUnitLabel), obsModel, null, null);
 		}
+
+		updateGridElementsSquare();
+		updateLockNumberOfRowsNCols();
 
 	}
 }
