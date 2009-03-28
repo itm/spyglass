bd9aac53eaf3b3b08c25cd18356531e9c1b2346d
Sebastian Ebers <ebers@itm.uni-luebeck.de>
2009-03-28 12:26:57 +0000

    TASK - # 216: ErrorDialog: Mehr Infos fixed http://www.danbim.de/flegsens/ticket/216

--- src/de/uniluebeck/itm/spyglass/gui/CheckboxErrorDialog.java
+++ src/de/uniluebeck/itm/spyglass/gui/CheckboxErrorDialog.java
@@ -4,7 +4,11 @@
  */
 package de.uniluebeck.itm.spyglass.gui;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
@@ -43,26 +47,8 @@ public class CheckboxErrorDialog extends ErrorDialog {
 	 * @param status
 	 *            the status object
 	 */
-	public CheckboxErrorDialog(final Shell parentShell, final String dialogTitle, final String message, final IStatus status) {
+	private CheckboxErrorDialog(final Shell parentShell, final String dialogTitle, final String message, final IStatus status) {
 		super(parentShell, dialogTitle, message, status, IStatus.OK | IStatus.INFO | IStatus.WARNING | IStatus.ERROR);
-	}
-
-	// --------------------------------------------------------------------------------
-	/**
-	 * Constructor
-	 * 
-	 * @param parentShell
-	 *            the parent shell
-	 * @param dialogTitle
-	 *            the dialog's title
-	 * @param message
-	 *            the message to be displayed
-	 * @param t
-	 *            an object of the type {@link Throwable} or <code>null</code>
-	 */
-	public CheckboxErrorDialog(final Shell parentShell, final String dialogTitle, final String message, final Throwable t) {
-		super(parentShell, dialogTitle, message, new Status(IStatus.ERROR, "unspecified ID", 0, "Status Error Message", t), IStatus.OK | IStatus.INFO
-				| IStatus.WARNING | IStatus.ERROR);
 	}
 
 	// --------------------------------------------------------------------------------
@@ -89,6 +75,7 @@ public class CheckboxErrorDialog extends ErrorDialog {
 		return dontShowAgain;
 	}
 
+	// --------------------------------------------------------------------------------
 	@Override
 	protected void createButtonsForButtonBar(final Composite parent) {
 		final Button button = new Button(parent, SWT.CHECK);
@@ -108,30 +95,7 @@ public class CheckboxErrorDialog extends ErrorDialog {
 		createDetailsButton(parent);
 	}
 
-	/**
-	 * Opens an error dialog to display the given error. Use this method if the error object being
-	 * displayed does not contain child items, or if you wish to display all such items without
-	 * filtering.
-	 * 
-	 * @param parent
-	 *            the parent shell of the dialog, or <code>null</code> if none
-	 * @param dialogTitle
-	 *            the title to use for this dialog, or <code>null</code> to indicate that the
-	 *            default title should be used
-	 * @param message
-	 *            the message to show in this dialog, or <code>null</code> to indicate that the
-	 *            error's message should be shown as the primary message
-	 * @param t
-	 *            on object of the type {@link Throwable} or <code>null</code>
-	 * @return {@link CheckboxErrorDialog#isDontShowAgain()}
-	 */
-	public static boolean openError(final Shell parent, final String dialogTitle, final String message, final Throwable t) {
-		final CheckboxErrorDialog d = new CheckboxErrorDialog(parent, dialogTitle, message, new Status(IStatus.ERROR, "unspecified ID", 0,
-				"Status Error Message", t));
-		d.open();
-		return d.isDontShowAgain();
-	}
-
+	// --------------------------------------------------------------------------------
 	/**
 	 * Opens an error dialog to display the given error. Use this method if the error object being
 	 * displayed does not contain child items, or if you wish to display all such items without
@@ -148,9 +112,58 @@ public class CheckboxErrorDialog extends ErrorDialog {
 	 * @return @return {@link CheckboxErrorDialog#isDontShowAgain()}
 	 */
 	public static boolean openError(final Shell parent, final String dialogTitle, final String message) {
-		final CheckboxErrorDialog d = new CheckboxErrorDialog(parent, dialogTitle, message, new Status(IStatus.ERROR, "unspecified ID", 0,
-				"Status Error Message", null));
+		final CheckboxErrorDialog d = new CheckboxErrorDialog(parent, dialogTitle, message, new Status(IStatus.ERROR, "unspecified ID", 0, "", null));
 		d.open();
 		return d.isDontShowAgain();
 	}
+
+	// --------------------------------------------------------------------------------
+	/**
+	 * Opens an error dialog to display the given error. Use this method if the error object being
+	 * displayed does not contain child items, or if you wish to display all such items without
+	 * filtering.
+	 * 
+	 * @param parent
+	 *            the parent shell of the dialog, or <code>null</code> if none
+	 * @param dialogTitle
+	 *            the title to use for this dialog, or <code>null</code> to indicate that the
+	 *            default title should be used
+	 * @param message
+	 *            the message to show in this dialog, or <code>null</code> to indicate that the
+	 *            error's message should be shown as the primary message
+	 * @param t
+	 *            on object of the type {@link Throwable}
+	 * @return {@link CheckboxErrorDialog#isDontShowAgain()}
+	 */
+	public static boolean openError(final Shell parent, final String dialogTitle, final String message, final Throwable t) {
+
+		if (t == null) {
+			return openError(parent, dialogTitle, message);
+		}
+
+		final Status[] childStatus = new Status[1];
+		childStatus[0] = new Status(IStatus.ERROR, "not_used", 0, "\r\n" + getStackTrace(t), null);
+		final MultiStatus m = new MultiStatus("not_used", 0, childStatus, t.getMessage(), t);
+
+		final CheckboxErrorDialog d = new CheckboxErrorDialog(parent, dialogTitle, message, m);
+		d.open();
+		return d.isDontShowAgain();
+	}
+
+	// --------------------------------------------------------------------------------
+	/**
+	 * Returns a {@link Throwable}'s stack trace as a string
+	 * 
+	 * @param t
+	 *            the {@link Throwable} which stack trace has to be returned
+	 * @return the stack trace of the given {@link Throwable}
+	 * 
+	 */
+	public static String getStackTrace(final Throwable t) {
+		final StringWriter stringWriter = new StringWriter();
+		final PrintWriter printWriter = new PrintWriter(stringWriter);
+		t.printStackTrace(printWriter);
+		return stringWriter.toString();
+	}
+
 }
