3f910ab855aad962c3af52cec4686713dfdafc00
Dariush Forouher <dariush@forouher.de>
2009-04-12 12:46:05 +0000

    - balloons should not autohide after a few seconds - balloons should stay with the control if the control moves

--- src/de/uniluebeck/itm/spyglass/gui/configuration/DatabindingErrorHandler.java
+++ src/de/uniluebeck/itm/spyglass/gui/configuration/DatabindingErrorHandler.java
@@ -16,8 +16,12 @@ import org.eclipse.core.databinding.observable.value.ValueChangeEvent;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.databinding.swt.ISWTObservable;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.ControlListener;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
@@ -33,11 +37,12 @@ import de.uniluebeck.itm.spyglass.util.SpyglassLoggerFactory;
  * @author Dariush Forouher
  *
  */
-public class DatabindingErrorHandler implements IValueChangeListener, DisposeListener {
+public class DatabindingErrorHandler implements IValueChangeListener, DisposeListener, ControlListener {
 
 	private static Logger log = SpyglassLoggerFactory.getLogger(DatabindingErrorHandler.class);
 
 	private final Map<Control, ToolTip> tipMap = new HashMap<Control, ToolTip>();
+	private final Map<ToolTip, Point> tipPos = new HashMap<ToolTip, Point>();
 
 	private final DataBindingContext dbc;
 
@@ -96,24 +101,47 @@ public class DatabindingErrorHandler implements IValueChangeListener, DisposeLis
 
 		if (status.isOK() && (tip != null)) {
 			log.debug("Content of widget "+c+" became good: "+status, status.getException());
-			tip.setVisible(false);
-			tip.dispose();
-			tipMap.remove(c);
+			destroyToolTip(c, tip);
 
 		} else if (!status.isOK()) {
 			log.debug("Content of widget "+c+" became bad: "+status, status.getException());
+
 			if (tip == null) {
-				tip = new ToolTip(shell, SWT.BALLOON | SWT.ICON_ERROR);
-				tipMap.put(c, tip);
-				c.addDisposeListener(this);
+				tip = createNewToolTip(c);
 			}
 
 			tip.setMessage(status.getMessage());
-			tip.setText("Bad input");
-			tip.setVisible(true);
-			tip.setAutoHide(false);
-			tip.setLocation(Display.getCurrent().map(c, null, c.getSize().x, c.getSize().y));
+			updatePosition(c, tip);
 		}
+	}
+
+	private void destroyToolTip(final Control c, final ToolTip tip) {
+		tip.dispose();
+		tipMap.remove(c);
+		if (!c.isDisposed()) {
+			c.removeDisposeListener(this);
+		}
+	}
+
+	private ToolTip createNewToolTip(final Control c) {
+		ToolTip tip;
+		tip = new ToolTip(shell, SWT.BALLOON | SWT.ICON_ERROR);
+		tipMap.put(c, tip);
+		c.addDisposeListener(this);
+		c.addControlListener(this);
+
+		// register control listener to all parent composites.
+		// this way we get notified if we need to move the balloon.
+		Composite parent = c.getParent();
+		while(parent != null) {
+			parent.addControlListener(this);
+			parent = parent.getParent();
+		}
+
+		tip.setText("Bad input");
+		tip.setAutoHide(false);
+
+		return tip;
 	}
 
 
@@ -125,10 +153,40 @@ public class DatabindingErrorHandler implements IValueChangeListener, DisposeLis
 	public void widgetDisposed(final DisposeEvent e) {
 		final ToolTip tip = tipMap.get(e.widget);
 		if (tip != null) {
-			tip.dispose();
-			tipMap.remove(e.widget);
+			destroyToolTip((Control)e.widget, tip);
 		}
 	}
 
+	// --------------------------------------------------------------------------------
+	/* (non-Javadoc)
+	 * @see org.eclipse.swt.events.ControlListener#controlMoved(org.eclipse.swt.events.ControlEvent)
+	 */
+	@Override
+	public void controlMoved(final ControlEvent e) {
+		for(final Control c: tipMap.keySet()) {
+			final ToolTip tip = tipMap.get(c);
+			updatePosition(c, tip);
+		}
 
+	}
+
+	private void updatePosition(final Control c, final ToolTip tip) {
+		final Point newPosition = Display.getCurrent().map(c, null, c.getSize().x, c.getSize().y);
+		final Point oldPosition = tipPos.get(tip);
+		if (!newPosition.equals(oldPosition)) {
+			log.debug("Moved tooltop for control "+c);
+			tip.setLocation(newPosition);
+			tipPos.put(tip, newPosition);
+		}
+		tip.setVisible(true);
+	}
+
+	// --------------------------------------------------------------------------------
+	/* (non-Javadoc)
+	 * @see org.eclipse.swt.events.ControlListener#controlResized(org.eclipse.swt.events.ControlEvent)
+	 */
+	@Override
+	public void controlResized(final ControlEvent e) {
+		// nothing to do
+	}
 }
