a1e551de39a55976c7fa8f6f57405074f4725727
Sebastian Ebers <ebers@itm.uni-luebeck.de>
2009-05-05 10:32:45 +0000

    PacketGenerator supports configurations where only one packet is sent at all

--- src/de/uniluebeck/itm/spyglass/packetgenerator/PacketGenerator.java
+++ src/de/uniluebeck/itm/spyglass/packetgenerator/PacketGenerator.java
@@ -19,44 +19,44 @@ import de.uniluebeck.itm.spyglass.util.SpyglassLoggerFactory;
  */
 @Root
 public class PacketGenerator {
-	
+
 	private static Logger log = SpyglassLoggerFactory.getLogger(PacketGenerator.class);
-	
+
 	/**
 	 * The generator.
 	 */
 	@Element
 	private final SampleChooser sampleChooser = new SampleChooser();
-	
+
 	/**
 	 * the number of packets generated per second.
 	 */
 	@Element
 	private int packetsPerSecond = 1;
-	
+
 	/**
 	 * the list of sinks, to which the packets are send to.
 	 */
 	@ElementList
 	private final ArrayList<Sink> sinks = new ArrayList<Sink>();
-	
+
 	/**
 	 * this methods sole existence is to convince, that the mentioned variable must not be final!
 	 */
 	private void methodToWorkaroundEclipse() {
 		packetsPerSecond = packetsPerSecond;
 	}
-	
+
 	/**
 	 * Is the generator paused?
 	 */
 	private volatile boolean paused = false;
-	
+
 	/**
 	 * When false, then the generator will shutdown within a short amount of time.
 	 */
 	private volatile boolean running = true;
-	
+
 	/**
 	 * pause the generator
 	 */
@@ -64,7 +64,7 @@ public class PacketGenerator {
 		log.info("Pausing the packet generator.");
 		this.paused = true;
 	}
-	
+
 	/**
 	 * Resume the generator
 	 */
@@ -72,34 +72,34 @@ public class PacketGenerator {
 		log.info("Resuming the packet generator.");
 		this.paused = false;
 	}
-	
+
 	/**
 	 * Stopp the generator
 	 */
 	public void shutdown() {
 		log.info("Shutting down the packet generator.");
 		this.running = false;
-		
+
 	}
-	
+
 	/**
 	 * @return true if the Generator is currently paused.
 	 */
 	public boolean isPaused() {
 		return this.paused;
 	}
-	
+
 	/**
 	 * Start the generator. this method will not terminate until an error happens or the generator
 	 * is killed by calling shutdown().
 	 */
 	public void run() throws Exception {
-		
+
 		log.info("Initalizing PacketGenerator.");
 		for (final Sink sink : this.sinks) {
 			sink.init();
 		}
-		
+
 		while (running) {
 			if (!paused) {
 				final Sample sample = this.sampleChooser.getRandomSample();
@@ -108,16 +108,20 @@ public class PacketGenerator {
 				}
 			}
 			try {
+				if (packetsPerSecond <= 0) {
+					break;
+				}
 				Thread.sleep(1000 / this.packetsPerSecond);
+
 			} catch (final InterruptedException e) {
 				Thread.currentThread().interrupt();
 			}
 		}
-		
+
 		for (final Sink sink : this.sinks) {
 			sink.shutdown();
 		}
 		log.info("PacketGenerator shutted down.");
 	}
-	
+
 }
