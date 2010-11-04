56e572ae34f6652fefe7a402bc1e13d92274d1f5
Daniel Bimschas <bimschas@itm.uni-luebeck.de>
2010-11-04 21:58:17 +0100

    ...

---
+++ core/src/main/java/de/uniluebeck/itm/spyglass/Constants.java
@@ -0,0 +1,9 @@
+package de.uniluebeck.itm.spyglass;
+
+
+public class Constants {
+
+	public static final String SPYGLASS_APP_NAME = "SpyGlass";
+	public static final String SPYGLASS_APP_VERSION = "1.0";
+
+}
